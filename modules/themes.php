13f8295412f765760c71f938c3dfe078204104bc
mission <dedwards@dejgaming.com>
2010-09-16 19:22:55 -0500

    straightened up themes.php

--- modules/themes.php
+++ modules/themes.php
@@ -2,7 +2,9 @@
 function themes()
 {
 $theme1 = explode('/', $_SESSION['theme']);
-echo "<div align='center'><form action='' method='post'><select name='settheme'><option>Select</option><option value='templates/default/style.css'>Default</option>
-</select><input type='submit' value='Set' /></form><br>Current: ".$theme1[1]."</div>";
+echo "<div align='center'><form action='' method='post'><select name='settheme'>";
+echo "<option>Select</option>";
+echo "<option value='templates/default/style.css'>Default</option>";
+echo "</select><input type='submit' value='Set' /></form><br>Current: ".$theme1[1]."</div>";
 }
 ?>
